e2242d08b19c3727b496647327ffb48f780f1bcc
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-12 19:51:20 -0300

    Templates fundamentos

--- workspace/meuProjeto/meuProjeto/views.py
+++ workspace/meuProjeto/meuProjeto/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Olá Mundo!')
+    minha_variavel = 'Programe Facil'
+    return render(request, 'index.html', {'minha_variavel': minha_variavel})
