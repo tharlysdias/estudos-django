8e3ff0fdeea112941aef797523d586a4eef2e2c0
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-12 16:06:50 -0300

    Registrando model no Admin

--- workspace/meuProjeto/clientes/models.py
+++ workspace/meuProjeto/clientes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# comandos
+# python manage.py makemigrations
+# python manage.py migrate
+class Cliente(models.Model):
+    name = models.CharField(max_length=70, null=False)
+    endereco = models.CharField(max_length=200, blank=False, null=False)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
