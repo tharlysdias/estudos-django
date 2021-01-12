94b1091e7996886136a123873ff115b0ae8771cd
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-12 19:38:29 -0300

    Personalizando o Django Admin

--- workspace/meuProjeto/clientes/admin.py
+++ workspace/meuProjeto/clientes/admin.py
@@ -3,7 +3,23 @@ from .models import Cliente, Telefone, CPF, Produto
 
 # Register your models here.
 
-admin.site.register(Cliente)
+# fields
+# list_display
+# list_filter
+# search_fields
+
+class ClienteAdmin(admin.ModelAdmin):
+    # Determina os campos que serão mostrados
+    fields = ('name', 'endereco')
+    # Determina quais os campos que serão mostrados no grid
+    list_display = ('id', 'name', 'endereco', 'email')
+    # Permite que você filtre os dados por um determinado campo
+    list_filter = ('produtos',)
+    # Cria uma forma de pesquisar os dados
+    search_fields = ('id', 'name', 'email', 'produtos__name')
+
+
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Telefone)
 admin.site.register(CPF)
 admin.site.register(Produto)
